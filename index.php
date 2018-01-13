da46ad436156d4a2b7ed55feeaf734a6333959a1
alblake <30480905+alblake@users.noreply.github.com>
2018-01-13 10:21:02 -0700

    Delete index.php
    no default php needed

--- index.php
+++
@@ -1,4 +0,0 @@
-<h1>Agile Yak</h1>
-
-<p>I love taking an agile approach to life and software projects!</p>
-<p>I never thought I would one day want to be an agile yak. What is an agile yak anyway? As a software professional I find myself turning to agility practices to get things done in a timely and high quality fashion.</p>
